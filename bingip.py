43043b01219f189b48bef79852b4e0f986e1e357
Marc Wickenden <marc.wickenden@gmail.com>
2012-01-03 17:20:43 +0000

    Tool for returning a list of domain names associated with an IP address

---
+++ bingip.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# Queries an IP address on Bing and returns all the domains associated
+#
+import sys
+from BeautifulSoup import BeautifulSoup
+import urllib2
+import urlparse
+import re
+
+ipaddr = sys.argv[1]
+cookie = "SRCHHPGUSR=NEWWND=0&NRSLT=50&SRCHLANG=&AS=1&ADLT=DEMOTE" # We set this so we get up to 50 results per page
+
+url = 'http://www.bing.com/search?q=ip%3A'+ipaddr+'&go=&qs=n&sk=&sc=8-26&form=QBRE&filt=all'
+opener = urllib2.build_opener()
+opener.addheaders.append(('Cookie', cookie))
+response = opener.open(url)
+html = response.read()
+
+soup = BeautifulSoup(html)
+divs = soup.findAll('div', attrs={'class':'sb_tlst'})
+domains = set()
+
+for div in divs:
+	url = BeautifulSoup(str(div)).find('a')['href']
+	hostname = urlparse.urlparse(url).hostname
+
+	domains.add(hostname)
+
+for i in domains:
+	print i
